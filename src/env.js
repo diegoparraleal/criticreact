7832b0c319045ef9f7a36e6ec9304edd8c62df0e
diegoparraleal <diego.parra.leal@gmail.com>
2021-03-12 11:22:49 -0500

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,3 +1,6 @@
+// NOTE: DO NOT COMMIT THIS FILE WITH API KEYS ON IT
+// Please stop tracking it from git using the following command locally
+// git update-index --assume-unchanged src/env.js
 export const SERVER_URL = "<SERVERURL>"
 export const GOOGLE_OAUTH_CLIENTID = "<CLIENTID>"
 export const GOOGLE_API_KEY = "<APIKEY>"
